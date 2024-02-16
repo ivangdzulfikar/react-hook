89c11d81de44f2cc8d7313ee40ae6054d7b783cd
ivangdzulfikar <ivangahmad@gmail.com>
2024-02-16 20:04:35 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import UseState from "./components/UseState";
+import UseEffect from "./components/UseEffect";
 
 function App() {
   return (
     <div style={{ textAlign: "center" }}>
       <h1 style={{ fontSize: "4rem", marginBottom: "-60px" }}>React Hook</h1>
-      <UseState title="useSate" />
+
+      <UseEffect title="useEffect" />
     </div>
   );
 }
---
+++ src/components/UseEffect.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+const UseEffect = ({ title }) => {
+  const [resourceType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourceType]);
+
+  return (
+    <>
+      <h2 style={{ fontSize: "4rem", textDecoration: "underline" }}>{title}</h2>
+      <div>
+        <button onClick={() => setResourceType("posts")}>Posts</button>
+        <button onClick={() => setResourceType("users")}>Users</button>
+        <button onClick={() => setResourceType("comments")}>Comments</button>
+
+        <ul>
+          {items.map((item) => {
+            return <li>{JSON.stringify(item)}</li>;
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default UseEffect;
